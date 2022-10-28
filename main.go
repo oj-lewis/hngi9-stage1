352ecc1b303d94e1df64632dae9e9106e8710cc9
oj-lewis <omietejohnlewis2@gmail.com>
2022-10-28 06:23:25 +0100

    added cors

--- main.go
+++ main.go
@@ -10,13 +10,21 @@ func main() {
 	r := http.NewServeMux()
 
 	r.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		resp := map[string]any{
-			"slackUsername": "o.j. lewis",
-			"backend": true,
-			"age": 22,
-			"bio": "tech ethusiast",
+		resp := struct{
+			SlackUsername string `json:"slackUsername"`
+			Backend bool `json:"backend"`
+			Age int `json:"age"`
+			Bio string `json:"bio"`
+		}{
+			SlackUsername: "o.j. lewis",
+			Backend: true,
+			Age: 22,
+			Bio: "tech ethusiast",
 		}
 
+		res.Header().Set("Content-Type", "application/json")
+		res.Header().Set("Access-Control-Allow-Origin", "*")
+		res.Header().Set("Access-Control-Allow-Methods", "Get")
 		json.NewEncoder(res).Encode(resp)
 	})
 
